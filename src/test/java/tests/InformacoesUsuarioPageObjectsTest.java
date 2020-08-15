6d04b60a3a4656147d8cef350861f42d9898b9fc
Marcio.Souza <marcio.messiassouza@gmail.com>
2020-08-15 16:24:05 -0300

    Atualização

--- src/test/java/tests/InformacoesUsuarioPageObjectsTest.java
+++ src/test/java/tests/InformacoesUsuarioPageObjectsTest.java
@@ -1,11 +1,19 @@
 package tests;
 
+import org.easetech.easytest.annotation.DataLoader;
+import org.easetech.easytest.annotation.Param;
+import org.easetech.easytest.runner.DataDrivenTestRunner;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
 import pages.LoginPage;
 import suporte.Web;
+
+import static org.junit.Assert.*;
+@RunWith(DataDrivenTestRunner.class)
+@DataLoader(filePaths = "InformacoesusuarioTestDataPageObjectsTest.csv")
 
 public class InformacoesUsuarioPageObjectsTest {
     private WebDriver navegador;
@@ -16,14 +24,22 @@ public class InformacoesUsuarioPageObjectsTest {
     }
 
     @Test
-    public void testAdicionarUmaInformacaoAdicionaldoUsuario(){
-        new LoginPage(navegador)
+    public void testAdicionarUmaInformacaoAdicionaldoUsuario(
+            @Param(name="login")String login,
+            @Param(name="senha")String senha,
+            @Param(name="tipo")String tipo,
+            @Param(name="contato")String contato,
+            @Param(name="mensagemEsperada")String mensagemEsperada
+    ){
+        String textoToast = new LoginPage(navegador)
                 .clickSignIn()
-                .fazerLogin("julio0001", "123456")
+                .fazerLogin(login, senha)
                 .clicarMe()
                 .clicarAbaMoreDataAboutYou()
-                .clicarNoBotaoAddMoreDataAboutYou();
-
+                .clicarNoBotaoAddMoreDataAboutYou()
+                .adicionarContato(tipo, contato)
+                .capturarTextoToast();
+    assertEquals(mensagemEsperada, textoToast);
     }
 
     @After
